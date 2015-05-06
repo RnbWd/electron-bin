a24782f609a43a84f8e98e0cf8a91801317d384f
RnbWd <dwisner6@gmail.com>
2015-05-06 14:32:05 -0700

    update

--- install.js
+++ install.js
@@ -2,13 +2,15 @@
 
 var os = require('os')
 var path = require('path')
-var nugget = require('nugget')
-var extract = require('extract-zip')
-var fs = require('fs')
-var pkg = require('./package.json');
-var getHomePath = require("home-path");
+// var nugget = require('nugget')
+// var extract = require('extract-zip')
+// var fs = require('fs')
+var Download = require('download')
+var pkg = require('./package.json')
+var getHomePath = require('home-path')()
 var platform = os.platform()
 var arch = os.arch()
+
 var version = pkg.electronVersion || '0.25.2'
 var filename = 'electron-v' + version + '-' + platform + '-' + arch + '.zip'
 var url = 'https://github.com/atom/electron/releases/download/v' + version + '/electron-v' + version + '-' + platform + '-' + arch + '.zip'
@@ -25,10 +27,17 @@ var paths = {
 
 if (!paths[platform]) throw new Error('Unknown platform: ' + platform)
 
-nugget(url, {target: filename, dir: __dirname, resume: true, verbose: true}, function (err) {
-  if (err) return onerror(err)
-  fs.writeFileSync(path.join(__dirname, 'path.txt'), paths[platform])
-  extract(path.join(__dirname, filename), {dir: path.join(__dirname, 'dist')}, function (err) {
-    if (err) return onerror(err)
+new Download({mode: '755', extract: true})
+  .get(url)
+  .dest(paths[platform])
+  .run(function(files) {
+    console.log(files);
   })
-})
+
+// nugget(url, {target: filename, dir: __dirname, resume: true, verbose: true}, function (err) {
+//   if (err) return onerror(err)
+//   fs.writeFileSync(path.join(__dirname, 'path.txt'), paths[platform])
+//   extract(path.join(__dirname, filename), {dir: path.join(__dirname, 'dist')}, function (err) {
+//     if (err) return onerror(err)
+//   })
+// })
