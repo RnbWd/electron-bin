1e8804ba8a65b531b4b3205482cd8f8e14ec4db6
RnbWd <dwisner6@gmail.com>
2015-05-06 15:14:52 -0700

    update

--- install.js
+++ install.js
@@ -2,6 +2,7 @@
 
 var os = require('os')
 var path = require('path')
+var pathExists = require('path-exists');
 var nugget = require('nugget')
 var extract = require('extract-zip')
 var fs = require('fs')
@@ -11,7 +12,8 @@ var platform = os.platform()
 var arch = os.arch()
 
 var version = pkg.electronVersion || '0.25.2'
-var filename = 'electron-v' + version + '-' + platform + '-' + arch + '.zip'
+var filename = 'electron-v' + version + '-' + platform + '-' + arch;
+var zipname = filename + '.zip'
 var url = 'https://github.com/atom/electron/releases/download/v' + version + '/electron-v' + version + '-' + platform + '-' + arch + '.zip'
 
 function onerror (err) {
@@ -19,17 +21,17 @@ function onerror (err) {
 }
 
 var paths = {
-  darwin: path.join(getHomePath, './.electron/Electron.app/Contents/MacOS/Electron'),
-  linux: path.join(getHomePath, './.electron/electron'),
-  win32: path.join(getHomePath, './.electron/electron.exe')
+  darwin: path.join(getHomePath, './.electron/'+filename+'/Electron.app/Contents/MacOS/Electron'),
+  linux: path.join(getHomePath, './.electron/'+filename+'/electron'),
+  win32: path.join(getHomePath, './.electron/'+filename+'/electron.exe')
 }
 
 if (!paths[platform]) throw new Error('Unknown platform: ' + platform)
-
-nugget(url, {target: filename, dir: __dirname, resume: true, verbose: true}, function (err) {
+console.log(pathExists.sync(paths[platform]));
+nugget(url, {target: zipname, dir: __dirname, resume: true, verbose: true}, function (err) {
   if (err) return onerror(err)
   fs.writeFileSync(path.join(__dirname, 'path.txt'), paths[platform])
-  extract(path.join(__dirname, filename), {dir: path.join(getHomePath, '.electron')}, function (err) {
+  extract(path.join(__dirname, zipname), {dir: path.join(getHomePath, './.electron/'+filename)}, function (err) {
     if (err) return onerror(err)
   })
 })
